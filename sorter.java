cb4997d2dad05896f01baac8b2633fdc3496951a
Mohammed-Alghanem <145454493+Mohammed-Alghanem@users.noreply.github.com>
2023-09-25 15:31:15 +0300

    added insertionSort

--- sorter.java
+++ sorter.java
@@ -5,10 +5,12 @@ import java.io.*;
 public class sorter {
     public static void main(String[] args) {
         int array[] = {1, 2, 3, 4, 5};
-        sort(array);
+        selectionSort(array);
+        System.out.println(Arrays.toString(array));
+        insertionsort(array);
         System.out.println(Arrays.toString(array));
     }
-    public static int[] sort ( int arr[]){
+    public static int[] selectionSort ( int arr[]){
         int n = arr.length;
         for (int i = 0; i < n - 1; i++) {
 
@@ -20,6 +22,21 @@ public class sorter {
             int temp = arr[min_idx];
             arr[min_idx] = arr[i];
             arr[i] = temp;
+        }
+        return arr;
+    }
+    public static int[] insertionsort(int arr[]) {
+        int n = arr.length;
+        for (int i = 1; i < n; ++i) {
+            int key = arr[i];
+            int j = i - 1;
+
+
+            while (j >= 0 && arr[j] > key) {
+                arr[j + 1] = arr[j];
+                j = j - 1;
+            }
+            arr[j + 1] = key;
         }
         return arr;
     }
